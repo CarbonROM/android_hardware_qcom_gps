b2ff76daa8c54050b612d84673ad4c00e368c01c
Humberto Borba <humberos@gmail.com>
2015-11-04 13:01:01 -0200

    gps: Fix build process for msm8994
    This patch fixes build process for msm8994
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Iedfc78fa9c627a245c9d535c2364a303cdb71ad5

--- Android.mk
+++ Android.mk
@@ -9,7 +9,7 @@ ifneq ($(filter msm8960 apq8064 ,$(TARGET_BOARD_PLATFORM)),)
 else ifneq ($(filter msm8974 ,$(TARGET_BOARD_PLATFORM)),)
     #For msm8974 target
     include $(call all-named-subdir-makefiles,msm8974)
-else ifneq ($(filter msm8084 ,$(TARGET_BOARD_PLATFORM)),)
+else ifneq ($(filter msm8084 msm8994 ,$(TARGET_BOARD_PLATFORM)),)
     #For msm8084 target
     include $(call all-named-subdir-makefiles,msm8084)
 else ifeq ($(filter msm8916,$(TARGET_BOARD_PLATFORM)),)
