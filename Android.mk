934a0041f5ec5472d4ed502fbf6f3921fc6b3476
Christian Oder <myself5@carbonrom.org>
2016-08-24 00:22:56 +0200

    Revert "Do not build GPS HAL for msm8916"
    This reverts commit 5c7552e789e4f039bebb09b972425a6cb47fc8e8.
    
    Change-Id: I74033662747db398e2725d9662f01c13c1dadd5a

--- Android.mk
+++ Android.mk
@@ -20,7 +20,7 @@ ifneq ($(TARGET_BOARD_AUTO),true)
       else ifneq ($(filter msm8996 ,$(TARGET_BOARD_PLATFORM)),)
         #For msm8996 target
         include $(call all-named-subdir-makefiles,msm8996)
-      else ifeq ($(filter msm8916,$(TARGET_BOARD_PLATFORM)),)
+      else
         #For all other targets
         GPS_DIRS=core utils loc_api platform_lib_abstractions etc
         include $(call all-named-subdir-makefiles,$(GPS_DIRS))
@@ -31,7 +31,7 @@ ifneq ($(TARGET_BOARD_AUTO),true)
         #For msm8909 target
         GPS_DIRS=msm8909/core msm8909/utils msm8909/loc_api msm8909/etc
         include $(call all-named-subdir-makefiles,$(GPS_DIRS))
-      else ifeq ($(filter msm8916 ,$(TARGET_BOARD_PLATFORM)),)
+      else
         GPS_DIRS=core utils loc_api platform_lib_abstractions etc
         include $(call all-named-subdir-makefiles,$(GPS_DIRS))
       endif
